1fbc25d587f0bcacf11b1b5b865f105891649423
Mario Molina <mam22696@login1.maverick2.tacc.utexas.edu>
2019-12-05 16:17:38 -0600

    training loop and graph code

--- dqn.py
+++ dqn.py
@@ -2,6 +2,8 @@
 #===================
 # Imports
 #===================
+import matplotlib
+matplotlib.use('Agg')
 
 import numpy as np
 import gym
@@ -10,7 +12,10 @@ import torch
 import warnings # For ignoring pytorch warnings
 import time
 import datetime
-import collections
+
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocatorimport collections
+
 
 #=====================
 # Network Declaration
@@ -47,10 +52,6 @@ class DQN_Network(nn.Module):
         feature_size = conv_output.view(1, -1).size(1)
         return feature_size
 
-    # TODO: update weights of DQN network
-    def update(self):
-        pass
-
 #===================
 # DQN Algorithm
 #===================
@@ -60,13 +61,80 @@ class DQN():
         self.experience_replay = # TODO
         self.network = DQN_Network()
         self.target_network = DQN_Network()
+        self.rewards = []
         pass
 
     # Return the Q-value of (s,a)
     def __call__(self, s, a):
         pass
+    
+    # Return Greedy Action
+    def get_action(self, s, epsilon):
+        pass
+    
+    # Keep Track of Rewards
+    def save_reward(self, r):
+        self.rewards.append(r)
 
+    # Save Network
+    def save_network(self):
+        pass
+
+    # Update Model Based on state
+    def update(self, state)
     # TODO: change reward function
     # TODO: use environment wrappers to clip rewards, skip frames, stack frames
     #       - https://github.com/openai/baselines/blob/master/baselines/common/atari_wrappers.py
+ 
+#==================
+# Training Loop
+#==================
+
+#%% md
+
+ ## Training Loop
+
+#%%
+
+# Run Time
+start=timer()
+
+log_dir = "log/"
+
+env_id = "PongNoFrameskip-v4"
+env    = make_atari(env_id)
+env    = wrap_deepmind(env, episode_life=True, clip_rewards=True, frame_stack=False, scale=True)
+
+# TODO DQN
+model = DQN()
+
+total_reward = 0
+epsilon = 0.1
+total_steps = 500000
+state = env.reset()
+for i_steps in range(1, total_steps):
+    action = model.get_action(observation, epsilon)
+    
+    prev_state = state
+    state, reward, done, _ = env.step(action)
+    state = None if done else observation
+
+    model.update(state)
+    
+    #Reward for Surviving (Therefore just 1 per time step)
+    total_reward += 1
+
+    if done:
+        state = env.reset()
+        model.save_reward(total_reward)
+        total_reward = 0
+
+    if i_steps % 10000 == 0:
+        model.save_network()
+        ax = plt.subplot(111)
+        ax.plot(range(len(model.rewards)), model.rewards, label='y = Reward')
+        plt.title('Total Reward per Episode')
+        ax.legend()
+        ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+        fig.savefig('plot.png')
 
