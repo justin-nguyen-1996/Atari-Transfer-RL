e37ad95b95b1b56478721c08bd7973bd09a7d7c2
Justin Nguyen <2014justinnguyen@gmail.com>
2019-12-05 19:19:40 -0600

    Fixed the rest of the runtime errors

--- dqn.py
+++ dqn.py
@@ -13,6 +13,7 @@ import warnings # For ignoring pytorch warnings
 import time
 import datetime
 import collections
+import random
 
 from baselines import bench
 from baselines.common.atari_wrappers import make_atari, wrap_deepmind
@@ -27,8 +28,12 @@ log_dir = "logs/"
 env_id = "PongNoFrameskip-v4"
 env = make_atari(env_id)
 env = wrap_deepmind(env, episode_life=True, clip_rewards=True, frame_stack=False, scale=True)
+env.observation_space = gym.spaces.box.Box(
+    env.observation_space.low[0, 0, 0],
+    env.observation_space.high[0, 0, 0],
+    [env.observation_space.shape[2], env.observation_space.shape[1], env.observation_space.shape[0]],
+    dtype=env.observation_space.dtype)
 
-# TODO DQN
 total_reward = 0
 epsilon = 0.1
 total_steps = 500000
@@ -60,11 +65,11 @@ class DQN_Network(nn.Module):
         self.fc2 = nn.Linear(512, self.num_actions)
 
     def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = F.relu(self.conv3(x))
+        x = torch.nn.functional.relu(self.conv1(x))
+        x = torch.nn.functional.relu(self.conv2(x))
+        x = torch.nn.functional.relu(self.conv3(x))
         x = x.view(x.size(0), -1) # Reshape to 1D vector for fully connected layers
-        x = F.relu(self.fc1(x))
+        x = torch.nn.functional.relu(self.fc1(x))
         x = self.fc2(x)
         return x
 
@@ -81,12 +86,13 @@ class DQN_Network(nn.Module):
 class DQN():
     def __init__(self):
         self.experience_replay = collections.deque(maxlen=experience_replay_size)
-        self.num_feats = env.observation_space.shape
+        self.state_dims = env.observation_space.shape
         self.num_actions = env.action_space.n
-        self.network = DQN_Network(self.num_feats,self.num_actions)
-        self.target_network = DQN_Network(self.num_feats,self.num_actions)
+        self.network = DQN_Network(self.state_dims, self.num_actions)
+        self.target_network = DQN_Network(self.state_dims, self.num_actions)
         self.optimizer = torch.optim.Adam(self.network.parameters(), lr=alpha)
         self.rewards = []
+        self.target_network_update_count = 0
 
     # Return the Q-value of (s,a)
     def __call__(self, s, a):
@@ -95,10 +101,11 @@ class DQN():
 
     # Return Greedy Action
     def get_action(self, s, epsilon):
-        if (np.random(0, 1) < epsilon):
-            return np.randint(0, env.action_space.n)
+        if (np.random.uniform(0, 1) < epsilon):
+            return np.random.randint(0, env.action_space.n)
         else:
             with torch.no_grad():
+                s = torch.tensor([s], device=device, dtype=torch.float)
                 q_values = self.network(s)
                 return np.argmax(q_values)
 
@@ -113,27 +120,25 @@ class DQN():
 
     def get_loss(self):
         # Grab random trajectories from experience replay
-        trajectories = np.random.sample(self.experience_replay, batch_size)
+        trajectories = random.sample(self.experience_replay, batch_size)
         # Unpack trajectories
         states, actions, rewards, next_states = zip(*trajectories)
         # Reshape and create tensors
-        states = torch.tensor(states, device=device, dtype=torch.float).view(batch_size, self.state_dims)
-        actions = torch.tensor(actions, device=device, dtype=torch.long).squeeze().view(-1, 1)
-        rewards = torch.tensor(rewards, device=device, dtype=torch.float).squeeze().view(-1, 1)
-        next_states = torch.tensor(next_states, device=device, dtype=torch.float).view(batch_size, self.state_dims)
+        states = torch.tensor(states, device=device, dtype=torch.float).view((batch_size,)+self.state_dims)
+        actions = torch.tensor(actions, device=device, dtype=torch.long).squeeze().view(-1,1)
+        rewards = torch.tensor(rewards, device=device, dtype=torch.float).squeeze().view(-1,1)
+        next_states = torch.tensor(next_states, device=device, dtype=torch.float).view((batch_size,)+self.state_dims)
         # Compute the TD error
+        q_values = self.network(states).gather(1, actions)
         with torch.no_grad():
-            q_values = self.network(states).gather(1, actions)
-            target_action = self.target_model(next_states).argmax()
-            target_q_values = self.target_model(next_states).gather(1, target_action)
+            target_action = self.target_network(next_states).argmax(dim=1).view(-1,1)
+            target_q_values = self.target_network(next_states).gather(1, target_action)
         td_error = rewards + gamma*target_q_values - q_values
         loss = (0.5 * td_error**2).mean()
         return loss
 
     # Update Model Based on state
     def update(self, s, a, r, sp):
-        # Save experience
-        self.experience_replay.append((s, a, r, sp))
         # Get loss
         loss = self.get_loss()
         # Backprop and update the weights
@@ -154,15 +159,24 @@ class DQN():
 # Run Time
 #start=timer()
 model = DQN()
+state = env.reset()
+state = state.transpose(2, 0, 1)
 for i_steps in range(1, total_steps):
-    action = model.get_action(observation, epsilon)
+    action = model.get_action(state, epsilon)
 
     prev_state = state
     state, reward, done, _ = env.step(action)
+    state = state.transpose(2, 0, 1)
     # TODO: change state_dims or pass in as correct size after state aggregation
-    state = None if done else observation
+    if done:
+        state = None
 
-    model.update(state)
+    # Save experience
+    model.experience_replay.append((prev_state, action, reward, state))
+
+    # Skip some frames to get some experiences in replay buffer
+    if i_steps >= 100:
+        model.update(prev_state, action, reward, state)
 
     # Reward for Surviving (Therefore just 1 per time step)
     total_reward += 1
@@ -171,6 +185,9 @@ for i_steps in range(1, total_steps):
         state = env.reset()
         model.save_reward(total_reward)
         total_reward = 0
+
+    if i_steps % 1000 == 0:
+        print(f'i_steps: {i_steps}')
 
     if i_steps % 10000 == 0:
         model.save_network()
