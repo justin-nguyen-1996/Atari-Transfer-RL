cf3bcd681ba7f76222833ad3b9ad12b6a5aff414
Justin Nguyen <2014justinnguyen@gmail.com>
2019-12-07 21:58:09 -0600

    Filled out more of the policy distill algo. Figure out kl divergence stuff and other kinks later

--- policy_distill.py
+++ policy_distill.py
@@ -129,3 +129,126 @@ class Policy_Distill(nn.Module):
         self.network = self.network.to(device)
         self.target_network = self.target_network.to(device)
 
+    # Return Greedy Action
+    def get_epsilon_greedy_action(self, s, epsilon, env):
+        if (np.random.uniform(0, 1) < epsilon):
+            return np.random.randint(0, env.action_space.n)
+        else:
+            with torch.no_grad():
+                s = torch.tensor([s], device=device, dtype=torch.float)
+                q_values = self.network(s)
+#                print(f'q_values: {q_values}')
+                a = q_values.argmax()
+                return a
+
+    # Keep Track of Rewards
+    def save_reward(self, r):
+        self.rewards.append(r)
+
+    # Save Network
+    def save_network(self):
+        torch.save(self.network.state_dict(), 'logs_distill/{}_saved_network.pt'.format(env_name))
+
+    def get_loss_dqn(self, experience_replay):
+        # Grab random trajectories from experience replay
+        trajectories = random.sample(experience_replay, batch_size)
+        # Unpack trajectories
+        states, actions, rewards, next_states = zip(*trajectories)
+        # Reshape and create tensors
+        states = torch.tensor(states, device=device, dtype=torch.float).view((batch_size,)+self.state_dims)
+        actions = torch.tensor(actions, device=device, dtype=torch.long).squeeze().view(-1,1)
+        rewards = torch.tensor(rewards, device=device, dtype=torch.float).squeeze().view(-1,1)
+        next_states = torch.tensor(next_states, device=device, dtype=torch.float).view((batch_size,)+self.state_dims)
+        # Compute the TD error
+        q_values = self.network(states).gather(1, actions)
+        with torch.no_grad():
+            target_action = self.target_network(next_states).argmax(dim=1).view(-1,1)
+            target_q_values = self.target_network(next_states).gather(1, target_action)
+        td_error = rewards + gamma*target_q_values - q_values
+        loss = (0.5 * td_error**2).mean()
+        return loss
+
+    def get_loss_kl_divergence(self):
+        pass
+
+    def get_loss(self):
+        return kl_divergence_ratio*get_loss_kl_divergence() + get_loss_dqn()
+
+    # Update Model Based on state
+    def update(self, s, a, r, sp):
+        # Get loss
+        loss = self.get_loss()
+#        print(f'loss: {loss}')
+        # Backprop and update the weights
+        self.optimizer.zero_grad()
+        loss.backward()
+        for param in self.network.parameters():
+            param.grad.data.clamp_(-1, 1)
+        self.optimizer.step()
+        # Update the target network every so often
+        self.target_network_update_count += 1
+        if self.target_network_update_count % target_network_update_freq == 0:
+            self.target_network.load_state_dict(self.network.state_dict())
+
+#==================
+# Training Loop
+#==================
+
+if __name__ == '__main__':
+    start = time.time()
+    model = DQN()
+    state = env.reset()
+    state = state.transpose(2, 0, 1)
+    total_reward = 0
+    for i_steps in range(1, total_steps):
+        action = model.get_epsilon_greedy_action(state, epsilon)
+        prev_state = state
+        state, reward, done, _ = env.step(action)
+        state = state.transpose(2, 0, 1)
+        print(f'action: {action}')
+
+        # TODO: change state_dims or pass in as correct size after state aggregation
+
+#        if done:
+#            state = None
+
+        # Save experience
+        model.experience_replay.append((prev_state, action, reward, state))
+
+        # Skip some frames to get some experiences in replay buffer
+        if i_steps >= 100:
+            model.update(prev_state, action, reward, state)
+
+        # Reward for Surviving (Therefore just 1 per time step)
+#        total_reward += 1 # TODO: change reward function
+        total_reward += reward
+
+        if done:
+            state = env.reset()
+            state = state.transpose(2, 0, 1)
+            model.save_reward(total_reward)
+            print(f'total_reward: {total_reward}')
+            total_reward = 0
+            model.save_network()
+            ax = plt.subplot(111)
+            ax.plot(range(len(model.rewards)), model.rewards, label='y = Reward')
+            plt.title('Total Reward per Episode')
+            ax.legend()
+            ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+            plt.savefig('plot.png')
+            plt.clf()
+            plt.close()
+
+        if i_steps % 100 == 0:
+            time_delta = int(time.time() - start)
+            time_delta = datetime.timedelta(seconds=time_delta)
+            print(f'i_steps: {i_steps}, time: {time_delta}')
+
+#        if i_steps % 10000 == 0:
+#            model.save_network()
+#            ax = plt.subplot(111)
+#            ax.plot(range(len(model.rewards)), model.rewards, label='y = Reward')
+#            plt.title('Total Reward per Episode')
+#            ax.legend()
+#            ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+#            fig.savefig('plot.png')
